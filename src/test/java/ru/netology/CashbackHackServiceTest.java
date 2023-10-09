dc0d98b29bbd82a844af71e5fa481425577f2e9b
Mirau Pavel <miraupavel@gmail.com>
2023-10-09 14:59:27 +0700

    Start Testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+class CashbackHackServiceTest {
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void remainLessThanBoundary() {
+
+        int actual = cash.remain(999);
+        int expected = 1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainLessThanBoundaryTwo() {
+
+        int actual = cash.remain(1);
+        int expected = 999;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainEqualToBoundary() {
+
+        int actual = cash.remain(1_000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainGreaterThanBoundary() {
+
+        int actual = cash.remain(1_500);
+        int expected = 500;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void remainLimitValueCheck() {
+
+        int actual = cash.remain(0);
+        int expected = 1_000;
+        Assert.assertEquals(actual, expected);
+    }
+}
